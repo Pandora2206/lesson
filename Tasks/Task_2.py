11bc615186bfbb858a852ab6e8178e31ee070177
GongiR <pakhomovd0@gmail.com>
2023-09-20 20:57:44 +0300

    Update Task again

--- Tasks/Task_2.py
+++ Tasks/Task_2.py
@@ -3,3 +3,12 @@
 является ли введенное слово палиндромом, 
 игнорируя пробелы и регистр символов (например, "А роза упала на лапу Азора").
 """
+
+def check_polindrom(string: str)-> bool:
+    string =string.replace(" ", '').lower()
+    return string == string[::-1]
+
+
+test_path = "А роза упала на лапу Азора"
+
+print(check_polindrom(test_path))
