c850cd49af1401d1f572d0050ab363a224aef088
GongiR <pakhomovd0@gmail.com>
2023-09-20 20:09:31 +0300

    Tasks

--- test.py
+++ test.py
@@ -1,16 +1,57 @@
-def list_sort(list_input):
-    list_abs = [abs(i) for i in list_input]
-    print(list_abs)
-    checker = False
-    for i in range(len(list_input)-1):
-        for j in range(len(list_input)-2):
-            if abs(list_input[j])>abs(list_input[j+1]):
-                checker = True  
-                list_input[j], list_input[j+1] = list_input[j+1], list_input[j]
-        if not checker:
-            return
+def get_task(n: int, k:int, colors: str) -> list[list[int]]:
+    
+    # офицеры - офицеры, ваше сердце под прицелом ... 
+    list_index = {i+1: color for i, color in enumerate(colors) }
+    count_w = colors.count('w')
 
-list = [1,2,3,-1,-2,-3]
-list_sort(list)
+    vzvods = []
+    index_w = [i for i in list(list_index.keys()) if list_index[i]=="w"]
 
-print(list)
+    while len(vzvods) < n//k+1:
+            # шаг вправо (беру всех)
+        index_ = index_w[0]    
+        vzvod = []
+        count = 0
+        for i in range(index_, index_ +k):
+            if list_index[i] =="v" and len(vzvod)<k:
+                vzvod.append(i)
+            else:
+                break
+        # шаг влево  
+        for i in reversed(list_index):
+            if len(vzvod)<k and list_index[i] =="v":
+                vzvod.append(i)
+            else:
+                break
+            
+
+        if len(vzvod)<k and :
+            # шаг вправо (беру всех)
+            index_ = index_w[-1]
+            vzvod = []
+            count = 0
+            last_index = list(list_index.keys())[-1]
+            for i in range(index_+1 , last_index+1):
+                if list_index[i] =="v" and len(vzvod)<k:
+                    vzvod.append(i)
+                else:
+                    break
+            # шаг влево  
+            for i in reversed(list(list_index.keys())[:index_-1]):
+                if len(vzvod)<k and list_index[i] =="v":
+                    vzvod.append(i)
+                else:
+                    break
+
+        if len(vzvod)==k:            
+            vzvod.append(index_)
+            index_w.remove(index_)
+            for item in vzvod:
+                list_index.pop(item)
+
+            vzvods.append(vzvod)
+
+ 
+
+test = [12, 2, "wwvwvvvvvvwv" ]
+get_task(*test)
