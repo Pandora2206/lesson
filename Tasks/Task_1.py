ba13c68a5136290d91f4fe057945b411220c0767
GongiR <pakhomovd0@gmail.com>
2023-09-20 20:28:05 +0300

    Update Task

--- Tasks/Task_1.py
+++ Tasks/Task_1.py
@@ -2,3 +2,9 @@
 Напишите программу, которая находит сумму всех чисел от 1 до N, где N вводится пользователем.
 """ 
 
+def sum_(n):
+    return sum([i for i in range(n+1)])
+
+n = 5 
+print(sum_(n))
+print(sum([i for i in range(n+1)]))
