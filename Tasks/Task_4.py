9b7bd2cc1ec86a571b11c2de773be51caf296738
GongiR <pakhomovd0@gmail.com>
2023-09-23 15:34:54 +0300

    New tasks

--- Tasks/Task_4.py
+++ Tasks/Task_4.py
@@ -1,3 +1,28 @@
 """
 Создайте список чисел и напишите программу, которая находит наименьшее и наибольшее число в списке.
 """
+
+import random 
+
+list_items = [random.randint(-100, 100) for _ in range(10)]
+print(list_items)
+
+
+def get_result(list_items):
+    min_ = min(list_items)
+    max_ = max(list_items)
+    index_min = 0
+    index_max = 0 
+    for i, item in enumerate(list_items):
+        
+        if item == min_:
+            index_min = i
+        if item == max_:
+            index_max = i
+
+    print(min_)
+    print(max_)
+    print(index_min)
+    print(index_max)
+
+get_result(list_items)
