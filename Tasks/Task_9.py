4de7ba94695b3d07831462e69a28967d4fcc552f
pandora2206 <halbrejn@gmail.com>
2023-10-04 20:54:37 +0300

    New Tasks

--- Tasks/Task_9.py
+++ Tasks/Task_9.py
@@ -1,17 +1,37 @@
-class triangle:
-    pass
-a=int(input())
-b=int(input())
-c=int(input())
-h=int(input())
-P=a+b+c
-S=a/2*h
-print(P)
-print(S)
-class  circle:
-    pass
-r=int(input())
-P=r*3.14*2
-S=3.14*r*r
-print(P)
-print(S)
+import math
+class Figure:
+    def calculate_P(self):
+        pass
+    def calculate_S(self):
+        pass
+
+class Rectangle(Figure):
+    line_1=0
+    line_2=0
+    def calculate_P(self):
+        return self.line_1*2+self.line_2*2
+    def calculate_S(self):
+        return self.line_1*self.line_2
+    def __init__(self,line_1,line_2):
+        self.line_1=line_1
+        self.line_2=line_2
+        self.P=self.calculate_P()
+        self.S=self.calculate_S()
+class Circle(Figure) :
+    r=0
+    def calculate_P(self):
+        return self.r*2*math.pi
+        pass
+    def calculate_S(self):
+        return self.r*self.r*math.pi
+        pass
+    def __init__(self,r):
+        self.r=r
+        self.P=self.calculate_P()
+        self.S=self.calculate_S()
+rect=Rectangle(10,5)
+print(rect.P,rect.S)
+circ=Circle(5)
+print(circ.P,circ.S)
+
+
