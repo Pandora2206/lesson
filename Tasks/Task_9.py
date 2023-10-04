19dcbce87b202d8e8c7dbe1b17df66b8e4d01b38
pandora2206 <halbrejn@gmail.com>
2023-10-04 19:19:10 +0300

    New Tasks

---
+++ Tasks/Task_9.py
@@ -0,0 +1,17 @@
+class triangle:
+    pass
+a=int(input())
+b=int(input())
+c=int(input())
+h=int(input())
+P=a+b+c
+S=a/2*h
+print(P)
+print(S)
+class  circle:
+    pass
+r=int(input())
+P=r*3.14*2
+S=3.14*r*r
+print(P)
+print(S)
