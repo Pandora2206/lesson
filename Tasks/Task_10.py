dffd22db7ec1293226b761995b11729fb7fe3818
pandora2206 <halbrejn@gmail.com>
2023-10-11 19:33:00 +0300

    New Tasks

---
+++ Tasks/Task_10.py
@@ -0,0 +1,23 @@
+class student:
+    name=""
+    age=""
+    grade=""
+def display_info(self):
+    pass
+def congratulate(self):
+    pass
+
+class student_1(student):
+    date="22.06"
+    name="Alex Crish"
+    grade ="9"
+    age="15"
+    birthday="22.05"
+def display_info(self):
+    return (self.name,self.grade,self.age)
+pass
+def congratulade(self):
+
+
+
+
