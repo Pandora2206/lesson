022f28bfba7865ecafc029d727efd6f75b4ca005
pandora2206 <halbrejn@gmail.com>
2023-10-11 19:40:20 +0300

    New Tasks

--- Tasks/Task_10.py
+++ Tasks/Task_10.py
@@ -8,7 +8,7 @@ def congratulate(self):
     pass
 
 class student_1(student):
-    date="22.06"
+    date="22.05"
     name="Alex Crish"
     grade ="9"
     age="15"
@@ -17,6 +17,9 @@ def display_info(self):
     return (self.name,self.grade,self.age)
 pass
 def congratulade(self):
+    if self.date==self.birthday:
+     self.age=self.age+1
+     print("с днём рождения")
 
 
 
